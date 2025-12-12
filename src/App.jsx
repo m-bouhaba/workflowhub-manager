3f5d7f5ca9f430ce9948a613d2a5c2e5e8d82b4b
salwa alla <salwaalla@gmail.com>
2025-12-12 09:17:07 +0100

    Update Footer et App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,89 @@
-import { useState } from "react";
- import "./App.css";
- import { BrowserRouter, Routes, Route } from "react-router-dom";
- import Home from "./Pages/Home";
- import Trash from "./Pages/Trash";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Trash from "./Pages/Trash";
 import Login from "./Pages/Login";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
- function App() {
-   return (
-     <>
-       <BrowserRouter>
-       <Navbar/> 
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/trash" element={<Trash />} />
-        </Routes>
-        
-      </BrowserRouter>
-     </>
-   );
- }
+function App() {
+  const location = useLocation();
 
- export default App;
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/trash" element={<Trash />} />
+      </Routes>
+
+      {location.pathname !== "/" && <Footer />}
+    </>
+  );
+}
+
+export default function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  import { useState } from "react";
+//   import "./App.css";
+//  import { BrowserRouter, Routes, Route } from "react-router-dom";
+//   import Home from "./Pages/Home";
+//   import Trash from "./Pages/Trash";
+//  import Login from "./Pages/Login";
+//  import Navbar from "./Components/Navbar";
+//  import Footer from "./Components/Footer";
+
+
+//   function App() {
+  
+//     return (
+//       <>
+//         <BrowserRouter>
+//         <Navbar/> 
+//          <Routes>
+//            <Route path="/" element={<Login />} />
+//            <Route path="/home" element={<Home />} />
+//           <Route path="/trash" element={<Trash />} />
+//         </Routes>
+        
+//          <Footer/> 
+//       </BrowserRouter>
+//      </>
+//    );
+//  }
+
+//  export default App;
